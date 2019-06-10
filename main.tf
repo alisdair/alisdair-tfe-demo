c5a63c622e50717242bf610aea57270115b581fb
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2019-06-10 09:11:12 -0400

    Terraform 0.12

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-variable "count" {
-  type = "string"
+variable "output_count" {
+  type    = number
   default = 100
 }
 
 resource "random_id" "random" {
-  count = "${var.count}"
+  count = var.output_count
 
-  keepers {
-    uuid = "${uuid()}"
+  keepers = {
+    uuid = uuid()
   }
 
   byte_length = 24
 }
 
 output "random_ids" {
-  value = ["${random_id.random.*.hex}"]
+  value = [random_id.random.*.hex]
 }
